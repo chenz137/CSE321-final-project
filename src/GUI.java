70c7c9af280c3376ab4b1085487b428c0fa481c5
oharaam3 <89606161+oharaam3@users.noreply.github.com>
2023-04-26 22:30:30 -0400

    Simplify GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,10 @@
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
 
 public class GUI {
 	public Dice d1;
@@ -12,11 +14,24 @@ public class GUI {
 	public Dice d5;
 	
 	public static void main(String args[]) {
-		Dice d1 = new Dice();
-		Dice d2 = new Dice();
-		Dice d3 = new Dice();
-		Dice d4 = new Dice();
-		Dice d5 = new Dice();
+		
+		//Create Dice
+		Dice dice1 = new Dice();
+		dice1.dice.setLocation(60,110);
+		
+		Dice dice2 = new Dice();
+		dice2.dice.setLocation(140,110);
+		
+		Dice dice3 = new Dice();
+		dice3.dice.setLocation(220,110);
+		
+		Dice dice4 = new Dice();
+		dice4.dice.setLocation(300,110);
+		
+		Dice dice5 = new Dice();
+		dice5.dice.setLocation(380,110);
+		
+		
 		
 		JFrame gui = new JFrame("Yahtzee");
 		gui.setBackground(Color.LIGHT_GRAY);
@@ -24,124 +39,6 @@ public class GUI {
 		JLabel yahtzee = new JLabel("<html><p style='font-size:20px'> Yahtzee </p></html>");
 		yahtzee.setBounds(200,50,100,40);
 		
-		
-		//Create First Dice
-		JButton db1 = new JButton(d1.toString());
-		db1.setBounds(60,130,60,60);
-		
-		//First Hold Function
-		JLabel h1 = new JLabel();
-		h1.setBounds(60,90,30,60);
-		db1.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (d1.hold) {
-					d1.unHold();
-					h1.setText("");
-				}
-				else {
-					d1.hold();
-					h1.setText("Held");
-				}
-				
-			}
-			
-		});
-		
-		
-		//Create Second Dice
-		JButton db2 = new JButton(d2.toString());
-		db2.setBounds(140,130,60,60);
-		
-		//Second Hold Function
-		JLabel h2 = new JLabel();
-		h2.setBounds(140,90,30,60);
-		db2.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (d2.hold) {
-					d2.unHold();
-					h2.setText("");
-				}
-				else {
-					d2.hold();
-					h2.setText("Held");
-				}
-				
-			}
-			
-		});
-		
-		
-		//Create Third Dice
-		JButton db3 = new JButton(d3.toString());
-		db3.setBounds(220,130,60,60);
-		
-		//Third Hold Function
-		JLabel h3 = new JLabel();
-		h3.setBounds(220,90,30,60);
-		db3.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (d3.hold) {
-					d3.unHold();
-					h3.setText("");
-				}
-				else {
-					d3.hold();
-					h3.setText("Held");
-				}
-				
-			}
-			
-		});
-		
-		
-		//Create the Fourth Dice
-		JButton db4 = new JButton(d4.toString());
-		db4.setBounds(300,130,60,60);
-		
-		//Fourth Hold Function
-		JLabel h4 = new JLabel();
-		h4.setBounds(300,90,30,60);
-		db4.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (d4.hold) {
-					d4.unHold();
-					h4.setText("");
-				}
-				else {
-					d4.hold();
-					h4.setText("Held");
-				}
-			}
-			
-		});
-		
-		
-		//Create the Fifth Dice
-		JButton db5 = new JButton(d5.toString());
-		db5.setBounds(380,130,60,60);
-		
-		//Fifth Hold Function
-		JLabel h5 = new JLabel();
-		h5.setBounds(380,90,30,60);
-		db5.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (d5.hold) {
-					d5.unHold();
-					h5.setText("");
-				}
-				else {
-					d5.hold();
-					h5.setText("Held");
-				}
-				
-			}
-			
-		});
 		
 		
 		
@@ -152,31 +49,21 @@ public class GUI {
 		roll.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				d1.roll();
-				d2.roll();
-				d3.roll();
-				d4.roll();
-				d5.roll();
-				db1.setText(d1.toString());
-				db2.setText(d2.toString());
-				db3.setText(d3.toString());
-				db4.setText(d4.toString());
-				db5.setText(d5.toString());
+				dice1.roll();
+				dice2.roll();
+				dice3.roll();
+				dice4.roll();
+				dice5.roll();
 			}
 		});
 		
 		//Add Elements
 		gui.add(yahtzee);
-		gui.add(db1);
-		gui.add(h1);
-		gui.add(db2);
-		gui.add(h2);
-		gui.add(db3);
-		gui.add(h3);
-		gui.add(db4);
-		gui.add(h4);
-		gui.add(db5);
-		gui.add(h5);
+		gui.add(dice1.dice);
+		gui.add(dice2.dice);
+		gui.add(dice3.dice);
+		gui.add(dice4.dice);
+		gui.add(dice5.dice);
 		gui.add(roll);
 		
 		//Make the Background
